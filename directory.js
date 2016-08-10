bd4e8e433e78124ede8b13421525b2ba6cdd521c
rajatmodi <rajat14modi@gmail.com>
2016-08-10 12:05:56 +0530

    Controller As

--- directory.js
+++ directory.js
@@ -1,8 +1,19 @@
 angular.module('directoryApp', [] )
-	.controller('directoryController', function($scope) {
-		$scope.list = [{name:'Scott' , age:20},
+	.controller('directoryController', function() {
+
+		var dirList = this;
+		dirList.list = [{name:'Scott' , age:20},
 				{name:'Ross' , age:21},
 				{name:'Ben' , age:22},
 				{name:'Courtney' , age:23}
-			]
+			];
+		dirList.addPerson = function(){
+			dirList.list.push({
+				name : dirList.name,
+				age : dirList.age
+			});
+
+		dirList.name='';
+		dirList.age= 0;
+		}
 	});
